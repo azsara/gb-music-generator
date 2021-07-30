c516870d5c1998bec681fe5bf121b6404efc156b
azsara <54192138+azsara@users.noreply.github.com>
2021-07-29 22:09:45 -0700

    Added the basics of random

--- audio.js
+++ audio.js
@@ -1,7 +1,8 @@
 "use strict";
 
 // Set the file we're going to open
-let url = "Audio/ADpickup.mod"
+//let url = "Audio/ADpickup.mod"
+let url = "Audio/ADpickup.mod";
 
 // This converts the numbers to strings of base-16 hex values.
 // If you can figure out how to make the columns more distinct, that would be neat.
@@ -15,25 +16,28 @@ function bufferToHex(buffer) {
 
 // This loads the mod file from disk, via an http request.
 // Don't forget to run a local webserver! python -m http.server
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; // NEW AZ
 let mod_file_request = new XMLHttpRequest();
 mod_file_request.open('get', url, true);
 
 // XMLHttpRequest can automatically convert our data into an ArrayBuffer
 mod_file_request.responseType = 'arraybuffer';
-
+console.log("Hello1");
 // The loading happens in a callback function, so it doesn't stop things from
 // happening on the page while the file loads.
 mod_file_request.onload = function(event){
 	//import .mod file
 	let trackerData = mod_file_request.response;
-
+	console.log("Hello2");
+	console.log(trackerData);
 	// If this is true, then the file has been loaded as
 	// an ArrayBuffer. For more on ArrayBuffers, consult https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer
 	if (trackerData) {
 		// The data is put into a ArrayBuffer view of type Uint8Array
 		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8Array
 		let byteData = new Uint8Array(trackerData);
-
+		console.log('Hello4');
+		console.log('Hello5')
 		// This is a fixed-length array of 8-bit numbers.
 
 
@@ -41,20 +45,46 @@ mod_file_request.onload = function(event){
 		let hex = bufferToHex(byteData)
 		document.getElementById("hexx").innerHTML = hex;
 		// Print the data to the console.
-		console.log(hex);
+		//console.log(hex);
 
 		// 1. locate the music pattern data
-
+	
 		// 2. edit the music pattern data
-
+		// Find the size of file
+		console.log(hex.length);
+		let s = ''
+		// TBD(Length of the new pattern)
+		for(let i = 0; i<11;i++){
+			function decimalToHexString(number)
+			{
+			if (number < 0)
+			{
+				number = 0xFFFFFFFF + number + 1;
+			}
+		
+			return number.toString(16).toUpperCase();
+			}
+			let new_s = decimalToHexString(Math.floor(Math.random()*255));
+			if(new_s.length == 1){
+				new_s = new_s.concat("0")
+			}
+			s = s.concat(new_s);
+			s = s.concat(" ");
+			
+		}
+		console.log(s);
+		// Replace the beginning of hex with random string
+		//TBD (Replace location)
+		hex = s + hex.substring(s.length);
 		// For random numbers, try Math.random()
 		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 		// 3 display the new file data
-
+		console.log(hex);
 		// 4. we won't write it out to disk yet, because that's actually going to be a separate part of the system
 		// Though if you really want to write it out to a file to listen to, you can.
 	}
 }
 mod_file_request.send();
 console.log(mod_file_request);
+console.log("Hello3");
