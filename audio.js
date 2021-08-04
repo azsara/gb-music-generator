042b92e5aacd6edee8487e17b091f93ef793230b
azsara <54192138+azsara@users.noreply.github.com>
2021-08-04 11:38:07 -0700

    randomwobble of a pattern of a given length

--- audio.js
+++ audio.js
@@ -14,6 +14,204 @@ function bufferToHex(buffer) {
 		.join("<br>\n"); // join the chunks together again, with a new linebreak between them
 }
 
+let Pattern = class {
+    constructor(p) {
+	    this.freq = 1;
+		this.pos = [];
+		this.pos.push(p); 
+	}
+        incr(p) {
+	        this.pos.push(p);
+	        this.freq = this.freq + 1;
+	}
+        getf() {
+	        return this.freq;
+	}
+        getp() {
+	        return this.pos;
+	}
+}
+
+var levelMap = new Map(); //Empty Map
+var splited;
+
+function locateOnePatterns() {
+
+    var map = new Map();
+
+	console.log("locateOnePatterns BEGIN");
+	
+	for (let i = 0; i < splited.length; i++) {
+	    var key = splited[i];
+		if (map.has(key)) {
+		    map.get(key).incr(i);
+		} 
+        else {
+		    let p = new Pattern(i);
+		    map.set(key, p);
+		}
+	}
+	
+	let repeats = 0;
+	let unique = 0;
+	for (const [key,value] of map){
+	    let f = map.get(key).getf();
+	    let p = map.get(key).getp();
+	    if (f == 1) {
+		    map.delete(key);
+		    unique += 1;
+	    } else {
+		    //console.log(`key: ${key}, value: ${f}, pos: ${p}`);
+		    repeats += 1;
+	    }
+	}
+
+	levelMap.set(1, map);
+	console.log(`locateOnePatterns: ${unique} ${repeats} END`);
+}
+
+function locateNextPatterns(level) {
+
+    //console.log("locateNextPatterns ${level} BEGIN");
+
+    var cmap = levelMap.get(level);
+    var nmap = new Map();
+
+    for (const [key,value] of cmap) {
+	    let f = cmap.get(key).getf();
+	    let p = cmap.get(key).getp();
+	    for (const pos of p) {
+		    if (pos + level > splited.length)
+		        break;
+		    var newkey = key + ' ' + splited[pos + level]; // add one more value
+            if (nmap.has(newkey)) {
+                nmap.get(newkey).incr(pos);
+            } else {
+                let newp = new Pattern(pos);
+                nmap.set(newkey, newp);
+            }     
+	}
+	// console.log(nmap.size)
+    }
+
+    let repeats = 0;
+    let unique = 0;
+    for (const [key,value] of nmap){
+	    let f = nmap.get(key).getf();
+	    let p = nmap.get(key).getp();
+	    if (f == 1) {
+	        nmap.delete(key);
+	        unique += 1;
+	    } else {
+	        // console.log(`key: ${key}, value: ${f}, pos: ${p}`);
+	        repeats += 1;
+	    }
+    }
+
+    levelMap.set(level + 1, nmap);
+    //console.log(`locateNextPatterns: ${level} ${unique} ${repeats} END`);
+
+    //console.log("locateNextPatterns END");
+}
+
+function locatePatterns(hex) {
+
+    hex = hex.replace(/\<br\>/g,"");
+    hex = hex.replace(/[\n\r]+/g, '');
+    splited = hex.split(" ");
+    console.log("locateAllPatterns BEGIN");
+    console.log(hex.length);
+    console.log(splited.length);
+
+	locateOnePatterns();
+	let total = 0;
+	for(let i = 1 ;; i++){
+	    locateNextPatterns(i);
+	    total += levelMap.get(i+1).size;
+	    console.log(`locateNextPatterns: ${i+1} ${levelMap.get(i+1).size} ${total} END`);
+	    if (levelMap.get(i).size == 0)
+		    break;
+	}
+
+	console.log("locateAllPatterns BEGIN");
+}
+
+function randompattern(len) {
+	var s = '';
+	for(let i = 0; i < len; i++){
+		function decimalToHexString(number) {
+			if (number < 0) {	
+				number = 0xFFFFFFFF + number + 1;
+			}
+			//onsole.log(number);
+			return number.toString(16).toLowerCase();
+		}
+
+		let new_s = decimalToHexString(Math.floor(Math.random()*255));
+		if(new_s.length == 1){
+			new_s = new_s.concat("0")
+		}
+		s = s.concat(new_s); //at the locations
+		s = s.concat(" "); //at the locations
+	}
+	console.log(`new pattern: ${s}`);
+	return s;
+}
+
+function wobblerandom(hex, len) {
+	//random repeating strings of length len, and replace those repeat strings with random values
+	var map = levelMap.get(len);
+	if (map.size == 0) {
+		console.log("Empty map; nothing done")
+		return;
+	}
+
+	// Pick a random string to replace
+	//index = Math.floor(Math.random()*map.size);
+	var index = 0; //replace the first pattern of the given length
+
+	var i = 0;
+	var indexkey = '';
+	for (const [key,value] of map){
+		if (i != index) {
+			i = i + 1;
+		}
+		indexkey = key;
+	}
+
+	var f = map.get(indexkey).getf();
+	var p = map.get(indexkey).getp();
+
+	console.log(`replace pattern: ${indexkey} at ${p}`);
+
+	var new_s = randompattern(len);
+
+	for (let j = 0; j < f; j++) {
+		let pos = p[j];
+		//console.log(pos)
+		let i = 0;
+		let k = 0;
+		while (pos > 0) {
+			i = i + 3;
+			pos = pos - 1;
+			k = k + 1;
+			if (k == 16) {
+				i = i + 5;
+				k = 0;
+			}
+		}
+		//console.log(new_s)
+		//console.log(new_s.length)
+		//console.log(i)
+		//console.log(hex.substring(0, i))
+		//console.log(hex.substring(i - 1 + new_s.length));
+		hex = hex.substring(0, i) + new_s + hex.substring(i + new_s.length);
+		//break;
+	}
+	console.log(hex)
+}
+
+
 // This loads the mod file from disk, via an http request.
 // Don't forget to run a local webserver! python -m http.server
 // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; // NEW AZ
@@ -46,12 +244,17 @@ mod_file_request.onload = function(event){
 		document.getElementById("hexx").innerHTML = hex;
 		// Print the data to the console.
 		//console.log(hex);
-
+		//console.log(parseInt("0x"+hex[0]+hex[1]));
 		// 1. locate the music pattern data
-	
+		locatePatterns(hex);
+
 		// 2. edit the music pattern data
 		// Find the size of file
-		console.log(hex.length);
+		wobblerandom(hex, 2) //replace pattern with length 2
+
+		// console.log(hex[hex.length-1]);
+		// console.log(hex[hex.length-2]);
+		/*
 		let s = ''
 		// TBD(Length of the new pattern)
 		for(let i = 0; i<11;i++){
@@ -61,7 +264,7 @@ mod_file_request.onload = function(event){
 			{
 				number = 0xFFFFFFFF + number + 1;
 			}
-		
+			console.log(number);
 			return number.toString(16).toUpperCase();
 			}
 			let new_s = decimalToHexString(Math.floor(Math.random()*255));
@@ -73,14 +276,17 @@ mod_file_request.onload = function(event){
 			
 		}
 		console.log(s);
+		console.log(parseInt("0x"+s[0]+s[1]));
 		// Replace the beginning of hex with random string
 		//TBD (Replace location)
 		hex = s + hex.substring(s.length);
+		*/
+
 		// For random numbers, try Math.random()
 		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 		// 3 display the new file data
-		console.log(hex);
+		// console.log(hex);
 		// 4. we won't write it out to disk yet, because that's actually going to be a separate part of the system
 		// Though if you really want to write it out to a file to listen to, you can.
 	}
